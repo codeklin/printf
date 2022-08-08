2228906402c3e8e27f18eeb88ba0191a44421152
Olajide <clickcom007@yahoo.com>
2022-08-08 06:20:42 -0700

    longoct

--- print_hex.c
+++ print_hex.c
@@ -1,28 +1,18 @@
-nclude "main.h"
+#include "main.h"
 
 /**
-
 * print_hex - prints a decimal in hexadecimal
-
 * @arguments: input string
-
 * @buf: buffer pointer
-
 * @ibuf: index for buffer pointer
-
 * Return: number of chars printed
-
 */
 
 int print_hex(va_list arguments, char *buf, unsigned int ibuf)
 
 {
-
 int int_input, i, isnegative, count, first_digit;
-
 char *hexadecimal, *binary;
-
-
 
 int_input = va_arg(arguments, int);
 
@@ -79,3 +69,6 @@ count++;
 free(binary);
 
 free(hexadecimal);
+return (count);
+
+}
