0ec5b3b795692256e7f8a8d3fddd49d7a8798827
Olajide <clickcom007@yahoo.com>
2022-08-08 03:47:41 -0700

    _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _putchar - prints a character
+* @c: teh character to be printed out
+*Return: 1;
+
+*/
+
+
+
+int _putchar(char c)
+
+{
+
+write(1, &c, 1);
+
+return (1);
+
+}
