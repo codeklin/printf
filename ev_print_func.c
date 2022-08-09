97cc9f971e9a4805259b7eda321f6138bbb44b0f
Olajide <clickcom007@yahoo.com>
2022-08-09 04:30:08 -0700

    advance

--- ev_print_func.c
+++ ev_print_func.c
@@ -1,17 +1,13 @@
 #include "main.h"
-
 /**
 * ev_print_func - returns the amount of identifiers.
 * @s: argument indentifier
 * @index: index of argument identifier.
 * Return: amount of identifiers.
 */
-
 int ev_print_func(const char *s, int index)
-
 {
 print_t pr[] = {
-
 {"c", print_chr}, {"s", print_str}, {"i", print_int},
 {"d", print_int}, {"b", print_bnr}, {"u", print_unt},
 {"o", print_oct}, {"x", print_hex}, {"X", print_upx},
@@ -31,33 +27,23 @@ print_t pr[] = {
 {"+ i", prinpint}, {"+ d", prinpint}, {" %", print_prg},
 {NULL, NULL},
 };
-
 int i = 0, j = 0, first_index;
-
 first_index = index;
-
 while (pr[i].type_arg)
 {
-
 if (s[index] == pr[i].type_arg[j])
 {
-
 if (pr[i].type_arg[j + 1] != '\0')
-
 index++, j++;
 else
-
 break;
 }
-
 else
 {
-
 j = 0;
 i++;
 index = first_index;
 }
-
 }
 return (j);
 }
